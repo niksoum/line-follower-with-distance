b0debc79d751a4045ac76081ec4b1db22d1e9f62
niksoum <90605744+niksoum@users.noreply.github.com>
2023-06-30 20:54:04 +0300

    Ενημέρωση pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 function ΕΥΘΕΙΑ () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80)
 }
 function ΑΡΙΣΤΕΡΑ_ () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 30)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
 }
 function ΠΙΣΩ () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
 }
 function ΔΕΞΙΑ_ () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 30)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
 }
 function ΣΤΑΜΑΤΑ () {
     maqueen.motorStop(maqueen.Motors.All)
 }
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 5) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 7) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             ΕΥΘΕΙΑ()
         } else {
